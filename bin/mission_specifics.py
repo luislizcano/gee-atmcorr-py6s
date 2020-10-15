d675db54d16c1fd988415c39bc0a18652e955122
Luis <49037033+luislizcano@users.noreply.github.com>
2020-10-15 11:42:30 -0400

    Add files via upload

---
+++ bin/mission_specifics.py
@@ -0,0 +1,264 @@
+"""
+mission_specifics.py, Sam Murphy (2017-06-28)
+
+Information on satellite missions stored here (e.g. wavebands, etc.)
+
+Modified by Luis Lizcano-Sandoval
+College of Marine Science, University of South Florida
+09-28-2020
+"""
+
+import ee
+
+
+def ee_bandnames(mission):
+    """
+    visible to short-wave infrared wavebands (EarthEngine nomenclature)
+
+    notes:
+    [1] skipped Landsat7 'PAN' to fit Py6S
+    [2] thermal bands in L7, L5 & L4 are not used in Py6S.
+    """
+
+    switch = {
+        'Sentinel-2A':['B1','B2','B3','B4','B5','B6','B7','B8','B8A','B9','B10','B11','B12'],
+        'Sentinel-2B':['B1','B2','B3','B4','B5','B6','B7','B8','B8A','B9','B10','B11','B12'],
+        'Landsat8':['B1','B2','B3','B4','B5','B6','B7','B8','B9'],
+        'Landsat7':['B1','B2','B3','B4','B5','B7'],
+        'Landsat5':['B1','B2','B3','B4','B5','B7'],
+        'Landsat4':['B1','B2','B3','B4','B5','B7']
+    }
+
+    return switch[mission]
+
+def py6s_bandnames(mission):
+    """
+    visible to short-wave infrared wavebands (Py6S nomenclature)
+
+    notes: 
+    [1] Landsat8 'B8' === 'PAN'
+    [2] Landsat7 'PAN' is missing?
+
+    """
+
+    switch = {
+        'Sentinel-2A':['B1','B2','B3','B4','B5','B6','B7','B8','B8A','B9','B10','B11','B12'],
+        'Sentinel-2B':['B1','B2','B3','B4','B5','B6','B7','B8','B8A','B9','B10','B11','B12'],
+        'Landsat8':['B1','B2','B3','B4','B5','B6','B7','B8','B9'],
+        'Landsat7':['B1','B2','B3','B4','B5','B7'],
+        'Landsat5':['B1','B2','B3','B4','B5','B7'],
+        'Landsat4':['B1','B2','B3','B4','B5','B7']
+    }
+
+    return switch[mission]
+
+def common_bandnames(mission):
+    """
+    visible to short-wave infrared wavebands (common bandnames)
+    """
+
+    switch = {
+        'Sentinel-2A':['aerosol','blue','green','red',
+        'redEdge1','redEdge2','redEdge3','nir','redEdge4',
+        'waterVapour','cirrus','swir1','swir2'],
+        'Sentinel-2B':['aerosol','blue','green','red',
+        'redEdge1','redEdge2','redEdge3','nir','redEdge4',
+        'waterVapour','cirrus','swir1','swir2'],
+        'Landsat8':['aerosol','blue','green','red','nir','swir1','swir2','pan','cirrus'],
+        'Landsat7':['blue','green','red','nir','swir1','swir2'],
+        'Landsat5':['blue','green','red','nir','swir1','swir2'],
+        'Landsat4':['blue','green','red','nir','swir1','swir2']
+    }
+
+    return switch[mission]
+
+def py6S_sensor(image,mission):
+    """
+    Py6S satellite_sensor name from satellite mission name.
+    *Modified to match either Sentinel-2A or Sentinel-2B.
+    """
+    switch = {
+        'Sentinel-2A':'S2A_MSI',
+        'Sentinel-2B':'S2B_MSI',
+        'Landsat8':'LANDSAT_OLI',
+        'Landsat7':'LANDSAT_ETM',
+        'Landsat5':'LANDSAT_TM',
+        'Landsat4':'LANDSAT_TM'
+    }
+    return switch[mission]
+
+def eeCollection(mission):
+    """
+    Earth Engine image collection name from satellite mission name
+    """
+
+    switch = {
+        'Sentinel2':'COPERNICUS/S2',
+        'Landsat8':'LANDSAT/LC08/C01/T1_TOA',
+        'Landsat7':'LANDSAT/LE07/C01/T1_TOA',
+        'Landsat5':'LANDSAT/LT05/C01/T1_TOA',
+        'Landsat4':'LANDSAT/LT04/C01/T1_TOA'
+    }
+
+    return switch[mission]
+
+def sunAngleFilter(mission):
+    """
+    Sun angle filter avoids where elevation < 15 degrees
+    """
+  
+    switch = {
+        'Sentinel-2A':ee.Filter.lt('MEAN_SOLAR_ZENITH_ANGLE',75),
+        'Sentinel-2B':ee.Filter.lt('MEAN_SOLAR_ZENITH_ANGLE',75),
+        'Landsat8':ee.Filter.gt('SUN_ELEVATION',15),
+        'Landsat7':ee.Filter.gt('SUN_ELEVATION',15),
+        'Landsat5':ee.Filter.gt('SUN_ELEVATION',15),
+        'Landsat4':ee.Filter.gt('SUN_ELEVATION',15)
+    }
+
+    return switch[mission]
+
+def ESUNs(image, mission, band):
+    """
+    ESUN (Exoatmospheric spectral irradiance)
+
+    References
+    ----------
+
+    Landsat 4  [1]
+    Landsat 5  [1]
+    Landsat 7  [1]
+    Landsat 8  [2]
+
+
+    [1] Chander et al. (2009) Summary of current radiometric calibration
+        coefficients for Landsat MSS, TM, ETM+, and EO-1 ALI sensors.
+        Remote Sensing of Environment. 113, 898-903
+
+    [2] Benjamin Leutner (https://github.com/bleutner/RStoolbox)
+    """
+  
+    # For Sentinel-2:
+    Sentinel2 = []
+    if 'Sentinel' in mission:
+        Sentinel2 =  float(image.get('SOLAR_IRRADIANCE_' + band).getInfo())
+    
+    # Coefficients for Landsat:
+    esunL8 = {
+        'B1': 1895.33, 
+        'B2': 2004.57, 
+        'B3': 1820.75, 
+        'B4': 1549.49, 
+        'B5': 951.76, 
+        'B6': 247.55, 
+        'B7': 85.46, 
+        'B8': 1723.8, 
+        'B9': 366.97
+    }
+    esunL7 = {
+        'B1': 1997, 
+        'B2': 1812, 
+        'B3': 1533, 
+        'B4': 1039, 
+        'B5': 230.8, 
+        'B7': 84.9, 
+    }
+    esunL5 = {
+        'B1': 1983, 
+        'B2': 1796, 
+        'B3': 1536, 
+        'B4': 1031, 
+        'B5': 220, 
+        'B7': 83.44, 
+    }    
+    esunL4 = {
+        'B1': 1983, 
+        'B2': 1795, 
+        'B3': 1539, 
+        'B4': 1028, 
+        'B5': 219.8, 
+        'B7': 83.49, 
+    }
+    
+    # Get the coefficient at the specified band:
+    Landsat8 = []
+    Landsat7 = [] # PAN =  1362 (removed to match Py6S)
+    Landsat5 = []
+    Landsat4 = []
+    if 'Landsat' in mission:
+        Landsat8 = esunL8[band]
+        Landsat7 = esunL7[band] # PAN =  1362 (removed to match Py6S)
+        Landsat5 = esunL5[band]
+        Landsat4 = esunL4[band]
+    
+    switch = {
+        'Sentinel-2A':Sentinel2,
+        'Sentinel-2B':Sentinel2,
+        'Landsat8':Landsat8,
+        'Landsat7':Landsat7,
+        'Landsat5':Landsat5,
+        'Landsat4':Landsat4
+    }
+
+    return switch[mission]
+
+def solar_z(image, mission):
+    """
+    solar zenith angle (degrees)
+    """
+
+    def sentinel2(image):
+        return image.get('MEAN_SOLAR_ZENITH_ANGLE').getInfo()
+  
+    def landsat(image):
+        return ee.Number(90).subtract(image.get('SUN_ELEVATION')).getInfo()
+  
+    switch = {
+        'Sentinel-2A':sentinel2,
+        'Sentinel-2B':sentinel2,
+        'Landsat8':landsat,
+        'Landsat7':landsat,
+        'Landsat5':landsat,
+        'Landsat4':landsat
+    }
+
+    getSolarZenith = switch[mission]
+
+    return getSolarZenith(image)
+
+
+def cloud_cover(mission):
+    """
+    cloud cover percentage
+    """
+
+    cloud_S2 = ['CLOUDY_PIXEL_PERCENTAGE']
+  
+    cloudLandsat = ['CLOUD_COVER']
+  
+    switch = {
+        'Sentinel-2A':cloud_S2,
+        'Sentinel-2B':cloud_S2,
+        'Landsat8':cloudLandsat,
+        'Landsat7':cloudLandsat,
+        'Landsat5':cloudLandsat,
+        'Landsat4':cloudLandsat
+    }
+
+    getCloudCover = switch[mission]
+
+    return getCloudCover
+
+
+def TOA(image, mission):
+
+    switch = {
+        'Sentinel-2A':image.divide(10000).set(image.toDictionary(image.propertyNames())),
+        'Sentinel-2B':image.divide(10000).set(image.toDictionary(image.propertyNames())),
+        'Landsat8':image,
+        'Landsat7':image,
+        'Landsat5':image,
+        'Landsat4':image
+    }
+
+    return switch[mission]
---
+++ bin/parameters.py
@@ -0,0 +1,274 @@
+"""
+Required parameters that changes among satellites:
+Set of parameters for atmospheric correction
+Adapted from Sam Murphy
+By Luis Lizcano-Sandoval
+09/28/2020
+"""
+
+import ee
+from Py6S import *
+import datetime
+import math
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.getcwd()),'bin'))
+from atmospheric import Atmospheric
+import mission_specifics as mn
+
+def BOA(mission,image,bandname):
+    
+    ##Load set of parameters:
+    image = ee.Image(image)
+    sensor = mn.py6S_sensor(image,mission)
+        
+    # Top of atmosphere reflectance:
+    toa = mn.TOA(image,mission)
+    
+    # Date in python format:
+    info = ee.Image(image).getInfo()['properties']
+    py_date = datetime.datetime.utcfromtimestamp(info['system:time_start']/1000)# i.e. Python uses seconds, EE uses milliseconds
+
+    # Solar zenith angle:
+    solar_z = mn.solar_z(image,mission)
+
+    # Target altitude:
+    ####Uncomment the next three lines if want to do AC to images over land.####
+    #SRTM = ee.Image('CGIAR/SRTM90_V4')# Shuttle Radar Topography mission covers *most* of the Earth
+    #alt = SRTM.reduceRegion(reducer = ee.Reducer.mean(),geometry = imgCentroid).get('elevation').getInfo()
+    #km = alt/1000 # i.e. Py6S uses units of kilometers
+    km = 0.001 #Set to 1m due to we are only interested in coastal water, not land.
+
+    # Date used for the Atmospheric correction functions, in GEE format:
+    ee_date = ee.Date(image.get('system:time_start'))
+
+    # Get the centroid coordinates of the image:
+    imgGeometry = image.geometry().buffer(10)
+    imgCentroid = imgGeometry.centroid()
+    coord = imgCentroid.getInfo()['coordinates'] #point only
+
+    # Predefined atmospheric constituents:
+    h2o = Atmospheric.water(imgCentroid,ee_date).getInfo()   #Water\
+    o3 = Atmospheric.ozone(imgCentroid,ee_date).getInfo()    #Ozone\
+    aot = Atmospheric.aerosol(imgCentroid,ee_date).getInfo() #Aerosols
+
+    # Instantiate
+    s = SixS()
+
+    # Atmospheric constituents\n",
+    s.atmos_profile = AtmosProfile.UserWaterAndOzone(h2o,o3)
+    s.aero_profile = AeroProfile.Continental
+    s.aot550 = aot
+
+    # Earth-Sun-satellite geometry
+    s.geometry = Geometry.User()
+    s.geometry.view_z = 0            # always NADIR (I think..)
+    s.geometry.solar_z = solar_z     # solar zenith angle
+    s.geometry.month = py_date.month # month and day used for Earth-Sun distance
+    s.geometry.day = py_date.day     # month and day used for Earth-Sun distance
+    s.altitudes.set_sensor_satellite_level()
+    s.altitudes.set_target_custom_altitude(km)
+
+
+    def spectralResponseFunction(bandname):
+
+        #Extract spectral response function for given band name
+        
+        if 'S2A_MSI' == sensor:
+            bandSelect = {
+                'B1':PredefinedWavelengths.S2A_MSI_01,
+                'B2':PredefinedWavelengths.S2A_MSI_02,
+                'B3':PredefinedWavelengths.S2A_MSI_03,
+                'B4':PredefinedWavelengths.S2A_MSI_04,
+                'B5':PredefinedWavelengths.S2A_MSI_05,
+                'B6':PredefinedWavelengths.S2A_MSI_06,
+                'B7':PredefinedWavelengths.S2A_MSI_07,
+                'B8':PredefinedWavelengths.S2A_MSI_08,
+                'B8A':PredefinedWavelengths.S2A_MSI_8A,
+                'B9':PredefinedWavelengths.S2A_MSI_09,
+                'B10':PredefinedWavelengths.S2A_MSI_10,
+                'B11':PredefinedWavelengths.S2A_MSI_11,
+                'B12':PredefinedWavelengths.S2A_MSI_12
+                }
+        elif 'S2B_MSI' == sensor:
+            bandSelect = {
+                'B1':PredefinedWavelengths.S2B_MSI_01,
+                'B2':PredefinedWavelengths.S2B_MSI_02,
+                'B3':PredefinedWavelengths.S2B_MSI_03,
+                'B4':PredefinedWavelengths.S2B_MSI_04,
+                'B5':PredefinedWavelengths.S2B_MSI_05,
+                'B6':PredefinedWavelengths.S2B_MSI_06,
+                'B7':PredefinedWavelengths.S2B_MSI_07,
+                'B8':PredefinedWavelengths.S2B_MSI_08,
+                'B8A':PredefinedWavelengths.S2B_MSI_8A,
+                'B9':PredefinedWavelengths.S2B_MSI_09,
+                'B10':PredefinedWavelengths.S2B_MSI_10,
+                'B11':PredefinedWavelengths.S2B_MSI_11,
+                'B12':PredefinedWavelengths.S2B_MSI_12
+                }
+        elif 'LANDSAT_OLI' == sensor:
+            bandSelect = {
+                'B1':PredefinedWavelengths.LANDSAT_OLI_B1,
+                'B2':PredefinedWavelengths.LANDSAT_OLI_B2,
+                'B3':PredefinedWavelengths.LANDSAT_OLI_B3,
+                'B4':PredefinedWavelengths.LANDSAT_OLI_B4,
+                'B5':PredefinedWavelengths.LANDSAT_OLI_B5,
+                'B6':PredefinedWavelengths.LANDSAT_OLI_B6,
+                'B7':PredefinedWavelengths.LANDSAT_OLI_B7,
+                'B8':PredefinedWavelengths.LANDSAT_OLI_B8,
+                'B9':PredefinedWavelengths.LANDSAT_OLI_B9
+                }
+        elif 'LANDSAT_ETM' == sensor:
+            bandSelect = {
+                'B1':PredefinedWavelengths.LANDSAT_ETM_B1,
+                'B2':PredefinedWavelengths.LANDSAT_ETM_B2,
+                'B3':PredefinedWavelengths.LANDSAT_ETM_B3,
+                'B4':PredefinedWavelengths.LANDSAT_ETM_B4,
+                'B5':PredefinedWavelengths.LANDSAT_ETM_B5,
+                'B7':PredefinedWavelengths.LANDSAT_ETM_B7
+                }
+        elif 'LANDSAT_TM' == sensor:
+            bandSelect = {
+                'B1':PredefinedWavelengths.LANDSAT_TM_B1,
+                'B2':PredefinedWavelengths.LANDSAT_TM_B2,
+                'B3':PredefinedWavelengths.LANDSAT_TM_B3,
+                'B4':PredefinedWavelengths.LANDSAT_TM_B4,
+                'B5':PredefinedWavelengths.LANDSAT_TM_B5,
+                'B7':PredefinedWavelengths.LANDSAT_TM_B7
+                }
+
+        return Wavelength(bandSelect[bandname])
+
+    def toa_to_rad(bandname):
+        
+        #Converts top of atmosphere reflectance to at-sensor radiance"
+        
+        # solar exoatmospheric spectral irradiance
+        ESUN = mn.ESUNs(image,mission,bandname)
+        solar_angle_correction = math.cos(math.radians(solar_z))
+
+        # Earth-Sun distance (from day of year)
+        doy = py_date.timetuple().tm_yday
+        # http://physics.stackexchange.com/questions/177949/earth-sun-distance-on-a-given-day-of-the-year
+        d = 1 - 0.01672 * math.cos(0.9856 * (doy-4)) 
+
+        # conversion factor
+        multiplier = ESUN*solar_angle_correction/(math.pi*d**2)
+
+        # at-sensor radiance
+        rad = toa.select(bandname).multiply(multiplier)
+    
+        return rad
+    
+
+    def surface_reflectance(bandname):
+        
+        #Calculate surface reflectance from at-sensor radiance given waveband name"
+         
+        # run 6S for this waveband
+        s.wavelength = spectralResponseFunction(bandname)
+        s.run()
+    
+        # extract 6S outputs
+        Edir = s.outputs.direct_solar_irradiance             #direct solar irradiance
+        Edif = s.outputs.diffuse_solar_irradiance            #diffuse solar irradiance
+        Lp   = s.outputs.atmospheric_intrinsic_radiance      #path radiance
+        absorb  = s.outputs.trans['global_gas'].upward       #absorption transmissivity
+        scatter = s.outputs.trans['total_scattering'].upward #scattering transmissivity
+        tau2 = absorb*scatter                                #total transmissivity
+
+        # radiance to surface reflectance
+        rad = toa_to_rad(bandname)
+        ref = rad.subtract(Lp).multiply(math.pi).divide(tau2*(Edir+Edif))
+
+        return ref
+    
+#     ## Function to get band scales
+        ##Update: It does not make sense to run these function to rescale bands,
+        ## because when the image is exported to Assets will be resampled to minimum res.
+#     def band_scale(bandname):
+        
+#         if 'S2A_MSI' == sensor:
+#             bandRes = {
+#                 'B1':60,
+#                 'B2':10,
+#                 'B3':10,
+#                 'B4':10,
+#                 'B5':20,
+#                 'B6':20,
+#                 'B7':20,
+#                 'B8':10,
+#                 'B8A':20,
+#                 'B9':60,
+#                 'B10':60,
+#                 'B11':20,
+#                 'B12':20
+#                 }
+#         elif 'S2B_MSI' == sensor:
+#             bandRes = {
+#                 'B1':60,
+#                 'B2':10,
+#                 'B3':10,
+#                 'B4':10,
+#                 'B5':20,
+#                 'B6':20,
+#                 'B7':20,
+#                 'B8':10,
+#                 'B8A':20,
+#                 'B9':60,
+#                 'B10':60,
+#                 'B11':20,
+#                 'B12':20
+#                 }
+#         elif 'LANDSAT_OLI' == sensor:
+#             bandRes = {
+#                 'B1':30,
+#                 'B2':30,
+#                 'B3':30,
+#                 'B4':30,
+#                 'B5':30,
+#                 'B6':30,
+#                 'B7':30,
+#                 'B8':15,
+#                 'B9':15
+#                 }
+#         elif 'LANDSAT_ETM' == sensor:
+#             bandRes = {
+#                 'B1':30,
+#                 'B2':30,
+#                 'B3':30,
+#                 'B4':30,
+#                 'B5':30,
+#                 'B7':30
+#                 }
+#         elif 'LANDSAT_TM' == sensor:
+#             bandRes = {
+#                 'B1':30,
+#                 'B2':30,
+#                 'B3':30,
+#                 'B4':30,
+#                 'B5':30,
+#                 'B7':30
+#                 }
+
+#         return (bandRes[bandname])
+    
+#     ## Function for resampling bands
+#     def resample(sr):
+#         ## Display a bilinear resampled image with 10m pixel spacing.
+#         band = sr.select(bandname)
+#         proj = band.projection()
+#         bandScale = band_scale(bandname)
+#         resample = sr.resample('bilinear').reproject({\
+#                 'crs': proj,
+#                 'scale': bandScale,
+#                 });
+#         return resample
+    
+    
+    
+    sr = surface_reflectance(bandname)
+#     sr_resample = resample(sr)
+    
+    
+    return sr
